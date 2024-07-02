6bc676805e8f3730c8ba4736366372466278b4a0
Derjai <ojaider@uninorte.edu.co>
2024-07-01 19:49:43 -0500

    feat: Implement email/password authentication in login screen

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -7,7 +8,67 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   bool passwordVisible = false;
+
+  Future<void> signInWithEmailAndPassword() async {
+    try {
+      final UserCredential userCredential =
+          await _auth.signInWithEmailAndPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+      if (userCredential.user != null && mounted) {
+        Navigator.pushNamed(context, '/home');
+      }
+    } on FirebaseAuthException catch (e) {
+      String errorMessage;
+      if (e.code == 'user-not-found') {
+        errorMessage = 'Usuario no encontrado';
+      } else if (e.code == 'wrong-password') {
+        errorMessage = 'Contraseña incorrecta';
+      } else {
+        errorMessage =
+            'Ocurrió un error al intentar iniciar sesión. Verifica las credenciales y tu conexión a internet';
+      }
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            content: Text(errorMessage),
+          ),
+        );
+      }
+    } catch (e) {
+      throw Exception('Error: $e');
+    }
+  }
+
+  Future<void> _sendPasswordResetEmail(String email) async {
+    try {
+      await _auth.sendPasswordResetEmail(email: email);
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content:
+                Text('Se ha enviado un correo para restablecer la contraseña'),
+          ),
+        );
+      }
+    } catch (e) {
+      if (mounted) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text(
+                'Error al enviar el correo para restablecer la contraseña'),
+          ),
+        );
+      }
+      throw Exception('Error: $e');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +94,7 @@ class _LoginScreenState extends State<LoginScreen> {
             style: TextStyle(fontSize: 16),
           ),
           TextFormField(
+            controller: _emailController,
             decoration: InputDecoration(
               labelText: 'Email',
               prefixIcon: const Icon(Icons.email),
@@ -48,6 +110,7 @@ class _LoginScreenState extends State<LoginScreen> {
           StatefulBuilder(
               builder: (BuildContext context, StateSetter setState) {
             return TextFormField(
+              controller: _passwordController,
               decoration: InputDecoration(
                 labelText: 'Contraseña',
                 prefixIcon: const Icon(Icons.lock),
@@ -74,7 +137,7 @@ class _LoginScreenState extends State<LoginScreen> {
           }),
           ElevatedButton(
             onPressed: () {
-              Navigator.pushNamed(context, '/home');
+              signInWithEmailAndPassword();
             },
             child: const Text(
               'Iniciar Sesión',
@@ -85,7 +148,15 @@ class _LoginScreenState extends State<LoginScreen> {
           const SizedBox(height: 16),
           TextButton(
             onPressed: () {
-              // TODO: Implement forgot password logic
+              if (_emailController.text.isNotEmpty) {
+                _sendPasswordResetEmail(_emailController.text);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(
+                    content: Text('Por favor, ingresa tu correo electrónico'),
+                  ),
+                );
+              }
             },
             child: const Text('¿Olvidaste tu contraseña?'),
           ),
